b587709847c2f93f835672fa07bd419c679efe3f
ghalebb <ghaleb.butto99@gmail.com>
2024-05-23 13:17:06 +0300

    edit deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -1,3 +1,44 @@
 #!/bin/bash
 
-# TODO your deploy script implementation...
+cd ~/app
+
+
+if [ ! -d "venv" ]; then
+  python3 -m venv venv
+fi
+
+
+source venv/bin/activate
+
+
+pip install -r requirements.txt
+
+
+deactivate
+
+# Create a systemd service file for the app
+SERVICE_FILE="/etc/systemd/system/netflix_movie_catalog.service"
+
+sudo bash -c "cat > $SERVICE_FILE" <<EOL
+[Unit]
+Description=Netflix Movie Catalog Service
+After=network.target
+
+[Service]
+User=ubuntu
+WorkingDirectory=/home/ubuntu/app
+ExecStart=/home/ubuntu/app/venv/bin/python /home/ubuntu/app/app.py
+Restart=always
+
+[Install]
+WantedBy=multi-user.target
+EOL
+
+# Reload systemd to apply the new service
+sudo systemctl daemon-reload
+
+# Enable the service to start on boot
+sudo systemctl enable netflix_movie_catalog.service
+
+# Start the service
+sudo systemctl start netflix_movie_catalog.service
